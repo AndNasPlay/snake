812670b415425198e6427090fd7f837d05e25fb4
AndNasPlay <57287272+AndNasPlay@users.noreply.github.com>
2020-04-26 15:30:34 +0300

    first char *

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -6,8 +6,18 @@ namespace Snake
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';
+
+            Draw(x1, y1, sym1);
+           
             Console.ReadLine();
+        }
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
