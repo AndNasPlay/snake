192f60545519b6fd9af012f58ab476d0db934511
AndNasPlay <57287272+AndNasPlay@users.noreply.github.com>
2020-04-28 21:48:05 +0300

    Добавление змейки

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -8,9 +8,12 @@ namespace Snake
 {
     class MainClass
     {
+        
         public static void Main(string[] args)
         {
 
+            Console.Clear();
+            Console.SetWindowSize(1, 1);
             HorizontalLine UpLine = new HorizontalLine(0, 60, 0, '+');
             UpLine.Drow();
             HorizontalLine DownLine = new HorizontalLine(0, 60, 15, '+');
@@ -19,6 +22,11 @@ namespace Snake
             LeftLine.Drow();
             VerticalLine RightLine = new VerticalLine(0, 15, 60, '+');
             RightLine.Drow();
+
+            Point p = new Point(5, 10, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drow();
+            snake.Move();
 
             Console.ReadLine();           
         
