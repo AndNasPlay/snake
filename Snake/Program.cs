d9148c02c63af4653493b392add7ab138c0e6965
AndNasPlay <57287272+AndNasPlay@users.noreply.github.com>
2020-04-29 21:59:29 +0300

    Работает

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -2,6 +2,7 @@
 
 namespace Snake
 {
+<<<<<<< Updated upstream
     class MainClass
     {
         public static void Main(string[] args)
@@ -11,3 +12,71 @@ namespace Snake
         }
     }
 }
+=======
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Console.SetBufferSize(79, 22);
+
+			Walls walls = new Walls(79, 22);
+			walls.Draw();
+
+			// Отрисовка точек			
+			Point p = new Point(4, 5, '*');
+			Snake snake = new Snake(p, 4, Direction.RIGHT);
+			snake.Draw();
+
+			FoodCreator foodCreator = new FoodCreator(79, 22, '$');
+			Point food = foodCreator.CreateFood();
+			food.Draw();
+
+			while (true)
+			{
+				if (walls.IsHit(snake) || snake.IsHitTail())
+				{
+					break;
+				}
+				if (snake.Eat(food))
+				{
+					food = foodCreator.CreateFood();
+					food.Draw();
+				}
+				else
+				{
+					snake.Move();
+				}
+
+				Thread.Sleep(100);
+				if (Console.KeyAvailable)
+				{
+					ConsoleKeyInfo key = Console.ReadKey();
+					snake.HandleKey(key.Key);
+				}
+			}
+			WriteGameOver();
+			Console.ReadLine();
+		}
+
+
+		static void WriteGameOver()
+		{
+			int xOffset = 25;
+			int yOffset = 8;
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.SetCursorPosition(xOffset, yOffset++);
+			WriteText("=========", xOffset, yOffset++);
+			WriteText("GAME OVER", xOffset + 1, yOffset++);
+			yOffset++;
+			WriteText("=========", xOffset, yOffset++);
+		}
+
+		static void WriteText(String text, int xOffset, int yOffset)
+		{
+			Console.SetCursorPosition(xOffset, yOffset);
+			Console.WriteLine(text);
+		}
+
+	}
+}
+>>>>>>> Stashed changes
